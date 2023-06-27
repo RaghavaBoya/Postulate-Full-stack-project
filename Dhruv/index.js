3848f23196ee67771c3ac670307d4793ddb3ca15
Dhruv Agrawal <106263552+developingdhruv@users.noreply.github.com>
2023-06-27 20:49:47 +0530

    user profile

--- Dhruv/index.js
+++ Dhruv/index.js
@@ -1,11 +1,7 @@
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
-
-signUpButton.addEventListener('click', () => {
-	container.classList.add("right-panel-active");
-});
-
-signInButton.addEventListener('click', () => {
-	container.classList.remove("right-panel-active");
-});
+$(document).ready(function(){
+    $('.jumbotron>h1').addClass("animated fadeInLeftBig");
+    $('.jumbotron>h2').addClass("animated fadeInRightBig");
+    $('.jumbotron>p').addClass("animated fadeInDownBig");
+    
+    
+  });
